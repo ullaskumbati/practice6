4e4727fa2821912ab50c0003e6bccf6873be8160
ullaskumbati <ullasbk.xworkz@gmail.com>
2021-12-20 21:24:30 +0530

    added Ar6

---
+++ Ar6.java
@@ -0,0 +1,14 @@
+class Ar6{
+	public static void main (String []a){
+		double[]fat ={4545.04045,62.567456,55.56556,67,54.544567,334.44456,33678.457659,-678.558768,-998.89767};
+		System.out.println(fat[0]);
+		System.out.println(fat[4]);
+		System.out.println(fat[2]);
+		System.out.println(fat[3]);
+		System.out.println(fat[1]);
+		System.out.println(fat[7]);
+		System.out.println(fat[8]);
+		System.out.println(fat[5]);
+		System.out.println(fat[6]);
+	}
+}
